795e1186c74213196ccd990c34ea94f7ec87518f
justdoit-4head <officialsoap11@gmail.com>
2023-02-16 20:21:19 +0530

    sss

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "*, https://cat-front.onrender.com",
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
